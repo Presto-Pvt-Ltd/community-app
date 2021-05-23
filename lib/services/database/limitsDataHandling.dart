898c3b3e2b274a4fa118006a3e6c3400ab3cc56d
Blazkowiz47 <sushrutpatwardhan@gmail.com>
2021-05-23 23:58:04 +0530

    Backbone added. Ready for writing functions

--- lib/services/database/limitsDataHandling.dart
+++ lib/services/database/limitsDataHandling.dart
@@ -1,5 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:presto/services/database/firestore.dart';
+import 'package:presto/services/database/firestoreBase.dart';
 
 enum LimitDocument {
   transactionLimits,
