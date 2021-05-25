6bc98efba037f86b0473cb2f45b4dc43c47ac3c7
Blazkowiz47 <sushrutpatwardhan@gmail.com>
2021-05-25 13:29:48 +0530

    Notifications refracted to Lend

--- lib/ui/views/home/lend/lend_viewModel.dart
+++ lib/ui/views/home/lend/lend_viewModel.dart
@@ -1,12 +1,23 @@
+import 'package:presto/app/app.locator.dart';
 import 'package:presto/app/app.logger.dart';
+import 'package:presto/services/authentication.dart';
+import 'package:presto/services/database/dataHandlers/notificationDataHandler.dart';
 import 'package:stacked/stacked.dart';
 
-class LendViewModel extends BaseViewModel {
+class LendViewModel extends StreamViewModel {
   final log = getLogger("LendViewModel");
+  final NotificationDataHandler _notificationDataHandler =
+      NotificationDataHandler();
+  final AuthenticationService _authenticationService =
+      locator<AuthenticationService>();
   String title = "I am Lend Screen";
   late void Function(bool) callback;
 
   void onModelReady(void Function(bool) callback) {
     this.callback = callback;
   }
+
+  @override
+  Stream get stream => _notificationDataHandler.getStream(
+      referralCode: _authenticationService.uid!);
 }
