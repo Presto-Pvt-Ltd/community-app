a0ec255c526fcafad7714546ddb5fc15056ed61e
Blazkowiz47 <sushrutpatwardhan@gmail.com>
2021-05-30 15:18:46 +0530

    data provider connected with view

--- lib/ui/views/home/borrow/borrow_viewModel.dart
+++ lib/ui/views/home/borrow/borrow_viewModel.dart
@@ -2,21 +2,23 @@ import 'package:presto/app/app.locator.dart';
 import 'package:flutter/material.dart';
 import 'package:presto/app/app.logger.dart';
 import 'package:presto/models/limits/transaction_limit_model.dart';
-import 'package:presto/models/user/transaction_data_model.dart';
 import 'package:presto/services/database/dataHandlers/limitsDataHandler.dart';
-import 'package:presto/services/database/dataProviders/user_data_provider.dart';
 import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
 
 class BorrowViewModel extends BaseViewModel {
   final log = getLogger("BorrowViewModel");
   String title = "I am Borrow";
-  TextEditingController amount = TextEditingController();
+  TextEditingController amountController = TextEditingController();
 
   final LimitsDataHandler _limitsDataHandler = locator<LimitsDataHandler>();
   TransactionLimits? transactionLimits;
 
   late void Function(bool) callback;
   bool gotData = false;
+
+  double amount = 0;
+
   void onModelReady(void Function(bool) callback) {
     log.v("Borrow View Model initiated");
     this.callback = callback;
@@ -34,5 +36,24 @@ class BorrowViewModel extends BaseViewModel {
     });
   }
 
-  void initiatePayment() {}
+  void initiatePayment() {
+    /// Confirm transaction intent
+    /// initiate transaction
+    /// ask for user preference in payment method
+    /// then create new transaction document in firebase and hive
+    /// update provider
+    /// send notifications and display some sort of timer
+    if (amount != 0)
+      locator<DialogService>()
+          .showConfirmationDialog(
+              title: "Confirmation",
+              description:
+                  "Are you sure you want to borrow and amount of \u2B09 $amount")
+          .then((value) {
+        if (value!.confirmed) {
+          /// initiate the process
+
+        }
+      });
+  }
 }
